96bf253c8edff6c64fab93fc35c22c680f15153c
HenrietteIraguha <h.iraguha@alustudent.com>
2026-02-23 12:37:34 -0800

    added 0-gather_data_from_an_API.py

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+if __name__ == "__main__":
+    employee_id = int(sys.argv[1])
+    base_url = "https://jsonplaceholder.typicode.com"
+
+    user = requests.get(f"{base_url}/users/{employee_id}").json()
+    todos = requests.get(f"{base_url}/todos", params={"userId": employee_id}).json()
+
+    name = user.get("name")
+    done = [t for t in todos if t.get("completed")]
+    total = len(todos)
+    num_done = len(done)
+
+    print(f"Employee {name} is done with tasks({num_done}/{total}):")
+    for task in done:
+        print(f"\t {task.get('title')}")
